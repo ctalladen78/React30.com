56be32b142d82d97ecee8b0e998c5943238d18e9
Michael Jackson <mjijackson@gmail.com>
2016-05-27 21:51:48 -0500

    Add favicon

--- modules/server/components/HomePage.js
+++ modules/server/components/HomePage.js
@@ -24,6 +24,7 @@ class HomePage extends React.Component {
           <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
           <meta name="timestamp" content={(new Date).toISOString()}/>
           <title>{title}</title>
+          <link rel="icon" type="image/x-icon" href="/favicon.ico?react30"/>
           <script dangerouslySetInnerHTML={{ __html: "window.Promise || document.write('\\x3Cscript src=\"/es6-promise.min.js\">\\x3C/script>\\x3Cscript>ES6Promise.polyfill()\\x3C/script>')" }}/>
           <script dangerouslySetInnerHTML={{ __html: "window.fetch || document.write('\\x3Cscript src=\"/fetch.min.js\">\\x3C/script>')" }}/>
           {styles.map(s => <link rel="stylesheet" key={s} href={s}/>)}
