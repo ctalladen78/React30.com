7736993bbc1d0eec96309db8990b75e0ce9259c7
Michael Jackson <mjijackson@gmail.com>
2016-05-27 22:29:23 -0500

    Add Twitter link in footer

--- modules/client/components/Home.js
+++ modules/client/components/Home.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import React30SquareURL from '../React30Square.png'
+import React30Logo from './React30Logo'
+import TwitterLogo from './TwitterLogo'
 import './Home.css'
 
 class Home extends React.Component {
   render = () => {
     return (
-      <div className="logo-wrapper">
-        <div
-          className="logo"
-          style={{ backgroundImage: `url(${React30SquareURL})` }}
-        />
+      <div className="logos-wrapper">
+        <div className="react30-logo">
+          <React30Logo/>
+        </div>
+        <div className="twitter-logo">
+          <a title="Follow React30 on Twitter" href="https://twitter.com/React30">
+            <TwitterLogo/>
+          </a>
+        </div>
       </div>
     )
   }
---
+++ modules/client/components/React30Logo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import React30SquareURL from '../React30Square.png'
+
+const React30Logo = (props) => {
+  const { style } = props
+
+  return (
+    <div {...props}
+      style={{
+        ...style,
+        backgroundImage: `url(${React30SquareURL})`,
+        backgroundSize: 'cover',
+        width: '100%',
+        height: '100%'
+      }}
+    />
+  )
+}
+
+export default React30Logo
